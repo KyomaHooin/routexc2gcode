d41b183778dce51c9d4a7f0dbaf007292a866c9a
KyomaHooin <kyomahooin@gmail.com>
2025-02-20 10:06:24 +0100

    Version update.

--- routexc2gcode.py
+++ routexc2gcode.py
@@ -3,37 +3,41 @@
 # autor: Vladimir Cerny
 # email: cernyv.pdp7@gmail.com
 # licence GPLv3
+# verze 1.1
 
 import os
+import datetime
 
 # definice promennych
 
 dir = "D:/cnc"
-print("Vychozi cesta D:\\cnc")
+print("Vychozi cesta zdroje D:\\cnc")
 zdrojovy = input("Zadej nazev zdrojoveho souboru: ")
-cela_cesta = os.path.join(dir,zdrojovy)
+cela_cesta_vstupu = os.path.join(dir,zdrojovy)
+dir2 = "D:/cnc/gcode"
+print("Vychozi cesta cile D:\\cnc\\gcode", "Existujici soubory prepise!", sep=os.linesep)
 vystupni = input("Zadej nazev vystupniho souboru: ")
-otacky = input("Zadej otacky vretene, [vychozi 25000]: ") or "25000"
+cela_cesta_vystupu = os.path.join(dir2,vystupni)
+otacky = input("Zadej otacky vretene, [vychozi 20000]: ") or "20000"
 feedrate = input("Zadej feedrate, [vychozi F300]: ") or "F300"
-safeZ = "40"
-moveZ = "Z5"
+moveZ = input("Zadej vysku prejezdu nad DPS, [vychozi 5]: ") or "5"
 milldepth = input("Zadej hloubku frezovani, [vychozi -2]: ") or "-2"
 nastroj_c = input("Zadej cislo nastroje, [vychozi 1]: ") or "1"
+safeZ = input("Zadej vysku zvednuti Z na konci programu, [vychozi 40]: ") or "40"
+today = datetime.datetime.now()
 
 # urceni vystupniho souboru
 
-out = open(f"{vystupni}", "w")
+out = open(f"{cela_cesta_vystupu}", "w")
+
+out.write("; "+f"{today}\n")
 
 hlavicka = """; Generovano v routexc2gcode.py
 
-; Absolutni koordinaty
-G90
-; Metricke
-G21
-; Spust vreteno
-M3 S"""+str(otacky)+"""
 ; Vyber nastroj
-T0"""+str(nastroj_c)+"""
+T"""+str(nastroj_c)+"""M6
+; Spust vreteno
+S"""+str(otacky)+"""M3
 ; Zvednuti Z po startu
 G00 Z5
 """
@@ -51,8 +55,10 @@ M2
 out.write(hlavicka)
 
 # pohyb vretene
+# moveZ ekvivaletni k M16 v exellonu
+# M15 se vyjadri, G00 nasleduje G01 se stejnou souradnici a pohybem Z dolu (obvykle)
 
-with open(f"{cela_cesta}") as f:
+with open(f"{cela_cesta_vstupu}") as f:
   line = f.readline()
   while line:
     line = f.readline()
@@ -67,7 +73,7 @@ with open(f"{cela_cesta}") as f:
       #print(milldepth, "; Vreteno dolu")
       #milldepth = "Z-2"
     if line.startswith("M16"):
-      out.write(moveZ+" "+"; Vreteno nahoru\n")
+      out.write("Z"+moveZ+" "+"; Vreteno nahoru\n")
     #if line.startswith("G40"):
     #  print(line.strip())
     if line.startswith("M30"):
