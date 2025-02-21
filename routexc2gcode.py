91defc9bbd8d0d367a75fe641dd2a75b499421f4
KyomaHooin <kyomahooin@gmail.com>
2025-02-21 18:04:52 +0100

    Detect and replace G00XY; cleanup.

--- routexc2gcode.py
+++ routexc2gcode.py
@@ -2,39 +2,36 @@
 #
 # Převodník routovacího Excellonu do G-code (běžných frézek)
 #
-#   autor: Vladimir Cerny
+#   autor: Vladimír Černý
 #   email: cernyv.pdp7@gmail.com
 # licence: GPLv3
-#   verze: 1.1
 #
 
-import os,sys
-import datetime
+import sys,os,re
+from datetime import datetime
 
 #
 # Promenne
 #
 
-cesta_vstupu = "D:/cnc"
-cesta_vystupu = "D:/cnc/gcode"
-
-today = datetime.datetime.now()
+VERSION='1.2'
+RUN=None
 
 #
 # Inicializace
 #
 
-print("\nroutexc2gcode\n")
+print("\nroutexc2gcode " + VERSION + "\n")
 
-print("Adresář zdroje [" + cesta_vstupu + "]\n")
-
+zdrojovy_adresar = input("[*] Název zdrojového adresáře [D:/cnc/]: ") or "D:/cnc/"
 zdrojovy_soubor = input("[*] Název zdrojového souboru: ")
-cela_cesta_vstupu = os.path.join(cesta_vstupu, zdrojovy_soubor)
 
-print("\nAdresář cíle [" + cesta_vystupu + "]\n")
+cela_cesta_vstupu = os.path.join(zdrojovy_adresar, zdrojovy_soubor)
 
-vystupni = input("[*] Název výstupního souboru: ")
-cela_cesta_vystupu = os.path.join(cesta_vystupu, vystupni)
+vystupni_adresar = input("[*] Název výstupního adresáře [D:/cnc/gcode/]: ") or "D:/cnc/gcode/"
+vystupni_soubor  = input("[*] Název výstupního souboru [test.gcode]: ") or "test.gcode"
+
+cela_cesta_vystupu = os.path.join(vystupni_adresar, vystupni_soubor)
 
 otacky = input("\n[*] Otáčky vřetene [20000]: ") or "20000"
 feedrate = input("[*] Feedrate [F300]: ") or "F300"
@@ -46,9 +43,9 @@ safeZ = input("[*] Výška zvednutí Z na konci programu [40]: ") or "40"
 hlavicka = """; Generováno v routexc2gcode.py
 
 ; Vyber nástroj
-T"""+str(nastroj_c)+"""M6
+T""" + str(nastroj_c) + """M6
 ; Spusť vřeteno
-S"""+str(otacky)+"""M3
+S""" + str(otacky) + """M3
 ; Zvednutí Z po startu
 G00 Z5
 """
@@ -57,23 +54,21 @@ paticka = """
 ; zastav vřeteno
 M5
 ; vyjetí vřetene a konec frézovaní
-G00 Z"""+str(safeZ)+"""
+G00 Z""" + str(safeZ) + """
 ; konec programu
 M2
 """
 
-start = input("\nPokračovat [y/n]: ")
-
-if start != "y": sys.exit(1)
+while RUN not in ('y','n'): RUN = input("\nPokračovat [y/n]: ")
+if RUN == "n": sys.exit(1)
 
 #
 # Hlavni kod
 #
 
 # urceni vystupniho souboru
-
 out = open(f"{cela_cesta_vystupu}", "w")
-out.write("; "+f"{today}\n")
+out.write("; " + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + "\n")
 
 # zapis hlavicky
 out.write(hlavicka)
@@ -82,22 +77,29 @@ out.write(hlavicka)
 # moveZ ekvivaletni k M16 v exellonu
 # M15 se vyjadri, G00 nasleduje G01 se stejnou souradnici a pohybem Z dolu (obvykle)
 
+# Posledni souradnice G01
+XY='XY'
+
 with open(f"{cela_cesta_vstupu}") as f:
   line = f.readline()
   while line:
     line = f.readline()
     if line.startswith("G00"):
-      out.write(line)
-      out.write("G01"+line.strip()[3:]+"Z"+milldepth+feedrate+"\n")
+      if line.strip() == 'G00XY':
+        out.write("G01" + re.sub('^(.*)\s+Z.*$','\\1', XY)) # odstraní Z souřadnici
+      else:
+        out.write(line)
+        out.write("G01" + line.strip()[3:] + "Z" + milldepth+feedrate + "\n")
     if line.startswith("G01"):
-      out.write(line[3:])
+      XY = line[3:]
+      out.write(XY)
     if line.startswith(("X","Y")):
       out.write(line)
     #if line.startswith("M15"):
       #print(milldepth, "; Vreteno dolu")
       #milldepth = "Z-2"
     if line.startswith("M16"):
-      out.write("Z"+moveZ+" "+"; Vreteno nahoru\n")
+      out.write("Z" + moveZ + " " + "; Vreteno nahoru\n")
     #if line.startswith("G40"):
     #  print(line.strip())
     if line.startswith("M30"):
